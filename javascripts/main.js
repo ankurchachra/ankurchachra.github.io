16586cba5c15e5c888e9dc655db9247d1bcb22ec
ankurchachra <ankurvmc@gmail.com>
2014-08-03 16:57:50 +0530

    commit
    a

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,8 @@ function buttonclicked()
 {
   var key1var = document.getElementById("key1").value;
   var res = key1var.toLowerCase().replace(/\s+/g, '').replace(/[^a-zA-Z-]/g, '');
-
+  var code1var = document.getElementById("code1").value;
+  var codecleaned1 = code1var.toLowerCase().replace(/\s+/g, '').replace(/[^a-zA-Z-]/g, '');
 var alphabetlist = new Array( 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' );
 var alphabetcount = new Array('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0' );
 
@@ -52,6 +53,7 @@ var answer1var='';
                filterkey1var+=alphabetlist[k];
              }
            }
+     answer1var = codecleaned1;
      
   document.getElementById("filterkey1").value=filterkey1var;
   document.getElementById("answer1").value=answer1var;
