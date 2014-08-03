1033476cbdfb318789697e2b3c42ae6a6b8fe760
ankurchachra <ankurvmc@gmail.com>
2014-08-03 15:51:20 +0530

    commit
    ac

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,6 +2,11 @@ function buttonclicked()
 {
   var key1var = document.getElementById("key1").value;
   var res = key1var.toLowerCase().replace(/\s+/g, '').replace(/[^a-zA-Z-]/g, '');
+  var key1Array = new Array();
+for (var i=0; i < res.length; i++){
+     key1Array.push(res[i]);
+}
+  
   if(res.length==0)
   {
     alert("Please enter the key before continuing");
