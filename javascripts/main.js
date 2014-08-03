79dda8bc1a71f27e13b725f3d6a7ac1cea5beee2
ankurchachra <ankurvmc@gmail.com>
2014-08-03 15:15:25 +0530

    commit
    ac

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,14 @@
 function buttonclicked()
 {
-  var key1var = document.getElementById("key1");
-  
+  var key1var = document.getElementById("key1").text;
+  if(key1var.length==0)
+  {
+    alert("Please enter the key before continuing");
+  }
+  else
+  {
   alert(key1var);
+  }
 }
 function button2clicked()
 {
