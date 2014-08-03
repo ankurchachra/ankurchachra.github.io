99aee341a3a029b1ffed4f5f59dbd172ac1e42e0
ankurchachra <ankurvmc@gmail.com>
2014-08-03 15:16:28 +0530

    commit
    ac

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,6 @@
 function buttonclicked()
 {
-  var key1var = document.getElementById("key1").text;
+  var key1var = document.getElementById("key1").value;
   if(key1var.length==0)
   {
     alert("Please enter the key before continuing");
