3ae5561d0c98be5eba015e61fe5314ec3bec2f92
ankurchachra <ankurvmc@gmail.com>
2014-08-03 15:56:47 +0530

    commit
    ac

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,10 +2,6 @@ function buttonclicked()
 {
   var key1var = document.getElementById("key1").value;
   var res = key1var.toLowerCase().replace(/\s+/g, '').replace(/[^a-zA-Z-]/g, '');
-  var key1Array = new Array();
-for (var i=0; i < res.length; i++){
-     key1Array.push(res[i]);
-}
 
 var alphabetlist = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 var alphabetcount = {'0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0' };
